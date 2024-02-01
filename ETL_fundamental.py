bd32cb266cfab080279a3aa738ef69e98c93df41
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2024-01-31 22:59:53 -0500

    Added comment to explaing boolean indexing

--- ETL_fundamental.py
+++ ETL_fundamental.py
@@ -22,6 +22,7 @@ def transform(data:dict) -> pd.DataFrame:
     
     #Use boolean indexing to get dataframe of name column contains California, 
     #df["name"].str.contains("California") will return series of boolean mask corresponding to row in dataframe
+    #df[df["name"].str.contains("California")]with this mask applied filters the DataFrame to only include rows where the mask is True
     df = df[df["name"].str.contains("California")]
     print(f"Number of universities in california {len(df)}")
     df['domains'] = [','.join(map(str, l)) for l in df['domains']]
